923d0649f503644db8572cdd46d5544ba731d706
Mifat123 <2010631170100@student.unsika.ac.id>
2022-10-22 09:47:03 +0700

    first commit

---
+++ tugas1/lib/soal1.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  String nama = "Muhammad Nurfathullah";
+  int npm = 20100;
+  double ip = 3.74;
+
+  print(nama);
+  print(npm);
+  print(ip);
+}
---
+++ tugas1/lib/soal3.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  List mahasiswa = ["Nama", "Muhammad Nurfathullah"];
+  Map<String, dynamic> data = {
+    "NPM": "2010631170100",
+    "Kelas": "5D",
+  };
+  var dataSet = {14, 04, 03};
+
+  print(mahasiswa);
+  print(data);
+  print(dataSet);
+}
